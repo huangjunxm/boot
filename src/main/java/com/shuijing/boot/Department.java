88035e6f774d80c9232512748c0d7c9f6dd86252
huangjunxm <24024127038@stu.hqu.edu.cn>
2025-08-28 21:44:12 +0800

    Mysql

---
+++ src/main/java/com/shuijing/boot/Department.java
@@ -0,0 +1,9 @@
+package com.shuijing.boot;
+
+public class Department {
+    private String id;
+
+    private String name;
+
+    private String tel;
+}
---
+++ src/main/java/com/shuijing/boot/MyBatisApplication1.java
@@ -0,0 +1,19 @@
+package com.shuijing.boot;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class MyBatisApplication1 {
+    public static void main(String[] args) throws Exception {
+        InputStream xml = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(xml);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        List<Department> departmentList = sqlSession.selectList("departmentMapper.findAll");
+        departmentList.forEach(System.out::println);
+    }
+}
